e128ce547b43696e8e8c8a91158fee0c7895a943
Rokibul Islam <rrcucse.11@gmail.com>
2020-02-02 00:11:10 +0600

    update

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,12 @@ var gulp = require('gulp');
 // Requires the gulp-sass plugin
 var sass = require('gulp-sass');
 var concat = require('gulp-concat');
+var jshint = require('gulp-jshint');
+var uglify   = require('gulp-uglify');
+var autoprefixer = require('autoprefixer');
+var	browserSync = require('browser-sync').create();
+var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
 // gulp.task('sass', function(){
 //   return gulp.src('app/scss/styles.scss')
 //     .pipe(sass()) // Converts Sass to CSS with gulp-sass
@@ -9,8 +15,38 @@ var concat = require('gulp-concat');
 // });
 
 gulp.task('sass', function() {
-  return gulp.src('app/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
-    .pipe(sass())
+  return gulp.src('app/src/scss/**/*.scss') // Gets all files ending with .scss in app/scss and children dirs
+    .pipe(sourcemaps.init())
+    .pipe(sass({
+    	outputStyle: 'expanded',
+		indentType: 'tab',
+		indentWidth: '1'
+    }).on('error', sass.logError))
+    .pipe(postcss([ autoprefixer() ]))
     .pipe(concat('main.css'))
     .pipe(gulp.dest('app/css/'))
 })
+
+gulp.task('javascript', function() {
+	return gulp.src('app/src/js/**/*.js')
+	.pipe(jshint())
+	.pipe(jshint.reporter('fail'))
+	.pipe(concat('main.js'))
+	.pipe(uglify())
+	.pipe(gulp.dest('app/js'));
+});
+
+
+// gulp.task('watch', function() {
+// 	// browserSync.init({
+// 	// 	open: 'external',
+// 	// 	proxy: 'http://localhost:8888/newwp/',
+// 	// 	port: 8080
+// 	// });
+// 	return gulp.watch('./app/src/scss/**/*.scss', ['sass']);
+// 	// gulp.watch('./app/src/js/**/*.js', ['javascript']);
+//     // gulp.watch('**/*').on('change', browserSync.reload);
+// });
+
+// gulp.task('default', ['sass']);
+// // gulp.task('default', ['sass', 'watch']);
